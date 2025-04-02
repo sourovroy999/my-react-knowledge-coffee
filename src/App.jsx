35714710ad96ed80d177f65b946c59dcc24d5320
sourovroy999 <sourovroy3050@gmail.com>
2025-04-03 03:43:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-red-500'>Vite + React</h1>
+      <h2 className='text-blue-300'>toyyy</h2>
+      <h2 className=''>
+
+      </h2>
+     
+    </>
+  )
+}
+
+export default App
